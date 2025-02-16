b35e8a6e82ae31105ed112c967dbee38b7872ee2
Rodrigo Luiz Cocate <rodrigo.cocate@hotmail.com>
2025-02-16 18:24:12 -0300

    Some unit tests of sale creation

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/CreateSaleHandlerTests.cs
@@ -0,0 +1,84 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.ORM.Repositories;
+using Ambev.DeveloperEvaluation.Unit.Application.TestData;
+using Ambev.DeveloperEvaluation.Unit.Domain;
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application
+{
+    public class CreateSaleHandlerTests
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMapper _mapper;
+        private readonly CreateSaleHandler _handler;
+
+        public CreateSaleHandlerTests()
+        {
+            _saleRepository = Substitute.For<ISaleRepository>();
+            _mapper = Substitute.For<IMapper>();
+            _handler = new CreateSaleHandler(_saleRepository, _mapper);
+        }
+
+        /// <summary>
+        /// Tests that a valid sale creation request is handled successfully.
+        /// </summary>
+        [Fact(DisplayName = "Given valid sale data When creating user Then returns success response")]
+        public async Task Handle_ValidRequest_ReturnsSuccessResponse()
+        {
+            // Given
+            var command = CreateSaleHandlerTestData.GenerateValidCommand();
+
+            Sale sale = new Sale
+            {
+                Id = Guid.NewGuid(),
+                SaleNumber = command.SaleNumber,
+                SaleDate = command.SaleDate,
+                CustomerId = command.CustomerId,
+                BranchId = command.BranchId,
+                TotalSaleAmount = command.TotalSaleAmount,
+                Status = command.Status,
+                Items = command.Items
+            };
+
+            var result = new CreateSaleResult
+            {
+                Id = sale.Id,
+            };
+
+            _mapper.Map<Sale>(command).Returns(sale);
+            _mapper.Map<CreateSaleResult>(sale).Returns(result);
+
+
+            // When
+            var createResult = await _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            createResult.Should().NotBeNull();
+            createResult.Id.Should().Be(sale.Id);
+            await _saleRepository.Received(1).CreateAsync(Arg.Any<Sale>(), Arg.Any<CancellationToken>());
+        }
+
+
+        /// <summary>
+        /// Tests that an invalid sale creation request throws a validation exception.
+        /// </summary>
+        [Fact(DisplayName = "Given invalid user data When creating sale Then throws validation exception")]
+        public async Task Handle_InvalidRequest_ThrowsValidationException()
+        {
+            // Given
+            var command = new CreateSaleCommand();
+
+            // When
+            var act = () => _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+        }
+    }
+}
---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/TestData/CreateSaleHandlerTestData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application.TestData
+{
+    /// <summary>
+    /// Provides methods for generating test data using the Bogus library.
+    /// This class centralizes all test data generation to ensure consistency
+    /// across test cases and provide both valid and invalid data scenarios.
+    /// </summary>
+    public static class CreateSaleHandlerTestData
+    {
+
+        //private static readonly Faker<SaleItem> itemFaker = new Faker<SaleItem>()
+        //    //.RuleFor(item => item.Id, f => f.IndexFaker)// f.IndexFaker + 1
+        //    .RuleFor(item => item.Name, f => f.Commerce.ProductName())
+        //    .RuleFor(item => item.Price, f => f.Commerce.Price())
+        //    .RuleFor(item => item.Quantity, f => f.Random.Int(1, 10));
+
+        private static readonly Faker<SaleItem> itemFaker = new Faker<SaleItem>();
+
+        private static readonly Faker<CreateSaleCommand> createSaleHandlerFaker = new Faker<CreateSaleCommand>()
+            .RuleFor(sale => sale.SaleNumber, f => f.Random.AlphaNumeric(5))
+            .RuleFor(sale => sale.SaleDate, f => f.Date.Recent(1))
+            .RuleFor(sale => sale.CustomerId, f => f.Random.Guid())
+            .RuleFor(sale => sale.BranchId, f => f.Random.Guid())
+            .RuleFor(sale => sale.TotalSaleAmount, f => f.Finance.Amount(1, 950))
+            .RuleFor(sale => sale.Status, f => f.PickRandom(SaleStatus.NotCancelled))
+            .RuleFor(sale => sale.Items, f => itemFaker.Generate(f.Random.Int(1, 10)));
+
+        public static CreateSaleCommand GenerateValidCommand()
+        {
+            return createSaleHandlerFaker.Generate();
+        }
+    }
+}
